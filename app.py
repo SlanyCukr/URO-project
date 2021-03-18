e67e85b57c846eccb65ada998b3f3cfac2a0a9b4
Marek Hovadík <hovadik@atlasgroup.cz>
2021-03-18 16:16:16 +0100

    Base for menu created, app doesn't start in maximum size

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
+from tkinter import Menu
+
+
 class App:
     def __init__(self, root):
         self.root = root
+
+        # menu
+        self.menubar = Menu(root)
+
+        self.filemenu = Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Nastavení", command=self.settings)
+        self.filemenu.add_command(label="Konec", command=self.quit)
+
+        self.view_menu = Menu(self.menubar, tearoff=0)
+        self.view_menu.add_command(label="Zobrazit na celou obrazovku", command=self.maximize)
+
+        self.menubar.add_cascade(label="Soubor", menu=self.filemenu)
+        self.menubar.add_cascade(label="Zobrazit", menu=self.view_menu)
+        self.menubar.add_command(label="Nápověda", command=self.help)
+        root.config(menu=self.menubar)
+
+    def quit(self):
+        pass
+
+    def settings(self):
+        pass
+
+    def maximize(self):
+        pass
+
+    def help(self):
+        pass
